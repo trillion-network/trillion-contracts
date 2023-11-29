e30737e37ac537cc748e215cd501ee992b42ff4d
Geng Sng <snggeng@gmail.com>
2023-11-29 11:48:22 -0500

    update FiatTokenV99 mock

--- test/mocks/FiatTokenV99.sol
+++ test/mocks/FiatTokenV99.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import {FiatTokenV1} from "../../src/v1/FiatTokenV1.sol";
 
+// mock contract used for testing upgrade
 contract FiatTokenV99 is FiatTokenV1 {
     function version() public pure override(FiatTokenV1) returns (string memory) {
         return "v99";
